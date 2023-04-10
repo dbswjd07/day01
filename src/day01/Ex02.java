7286914616217c85af4bcbe59b5218a75274c5fc
test123 <test123@naver.com>
2023-04-10 14:04:52 +0900

    day01 저장

---
+++ src/day01/Ex02.java
@@ -0,0 +1,27 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		/*
+		 escape 문자
+		 - 문자열 안에서 특수한 기능을 하는것.
+		 - 문자열은 " "(큰따옴표)로 묶여있는 값
+		 - \n : 엔터와 비슷하다
+		 - \t : tap(스페이스바 8칸정도)만큼 오른쪽 이동
+		 - \\ : \ 하나 표현
+		 - \" : " 하나 표현
+		 */
+		
+//		System.out.println("안\n녕\n하\n세\n요");
+//		System.out.println("1\t234567\t12345678\t1");
+//		
+//		System.out.println("이름\t나이\t");
+//		System.out.println("홍길동\t20살\t");
+		
+//		System.out.println(" \\ ");
+//		System.out.println("D:\\오전취업반_배윤정");
+		
+		System.out.println(" \"Hello\" ");
+		
+	}
+}
